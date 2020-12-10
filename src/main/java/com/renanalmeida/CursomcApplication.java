806f0b75748f863704199bf7d4cda421db7e68c4
Renan Almeida <renan.almeida.goncalves@everis.com>
2020-12-10 16:09:26 -0300

    Inserção da Classe Produto e efetuado relacionamento com Categoria

--- src/main/java/com/renanalmeida/CursomcApplication.java
+++ src/main/java/com/renanalmeida/CursomcApplication.java
@@ -8,14 +8,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.renanalmeida.domain.Categoria;
+import com.renanalmeida.domain.Produto;
 import com.renanalmeida.repositories.CategoriaRepository;
+import com.renanalmeida.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner{
 
 	//Autowired para ser instanciado automaticamente
 	@Autowired
-	private CategoriaRepository repo;
+	private CategoriaRepository categoriaRepository;
+	@Autowired
+	private ProdutoRepository produtoRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
@@ -27,9 +32,23 @@ public class CursomcApplication implements CommandLineRunner{
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
 		
+		//Instanciando categorias e produtos.
+		Produto prod1 = new Produto(null,"Computador", 2000.00);
+		Produto prod2 = new Produto(null,"Impressora", 800.00);
+		Produto prod3 = new Produto(null,"Impressora", 80.00);
+		
+		//Vinculando as categorias aos produtos.
+		cat1.getProdutos().addAll(Arrays.asList(prod1,prod2,prod3));
+		cat2.getProdutos().addAll(Arrays.asList(prod2));
+		//Vinculando os produtos as categorias.
+		prod1.getCategorias().addAll(Arrays.asList(cat1));
+		prod2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		prod3.getCategorias().addAll(Arrays.asList(cat1));
+		
 		//Dentro do método correspondente a gravação dos objetos no banco, foi criado
 		//um arraylist para inserir os valores dos objetos instanciados.
-		repo.saveAll(Arrays.asList(cat1, cat2));
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(prod1,prod2,prod3));
 		
 	}
 
--- src/main/java/com/renanalmeida/domain/Categoria.java
+++ src/main/java/com/renanalmeida/domain/Categoria.java
@@ -1,11 +1,14 @@
 package com.renanalmeida.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 @Entity  
 public class Categoria implements Serializable{	
@@ -15,6 +18,10 @@ public class Categoria implements Serializable{
 	@GeneratedValue(strategy=GenerationType.IDENTITY) //Estratégia para gerar ids automaticamente
 	private Integer id;
 	private String nome;
+	//Criar uma lista de produtos pois uma categoria possui um ou mais produtos.
+	//É feito exatamente o mesmo mapeamento que foi feito na classe Produtos que, por sua vez,  foi feito dentro do atributo categorias
+	@ManyToMany(mappedBy = "categorias")
+	private List<Produto> produtos = new ArrayList<>();
 	
 	public Categoria() {}
 
@@ -38,6 +45,15 @@ public class Categoria implements Serializable{
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+	
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
+	};
+	
 
 	@Override
 	public int hashCode() {
@@ -59,9 +75,6 @@ public class Categoria implements Serializable{
 		if (id != other.id)
 			return false;
 		return true;
-	};
-	
-	
-	
+	}
 
 }
