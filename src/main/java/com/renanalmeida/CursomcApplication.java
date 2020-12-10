f077a9aeb08e08b36809251eff1bf896dd562f4a
Renan Almeida <renan.almeida.goncalves@everis.com>
2020-12-10 08:56:29 -0300

    Instanciando o objeto Categoria e inserindo um valores em Arraylist

--- src/main/java/com/renanalmeida/CursomcApplication.java
+++ src/main/java/com/renanalmeida/CursomcApplication.java
@@ -1,13 +1,36 @@
 package com.renanalmeida;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.renanalmeida.domain.Categoria;
+import com.renanalmeida.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner{
+
+	//Autowired para ser instanciado automaticamente
+	@Autowired
+	private CategoriaRepository repo;
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		//O id, que é o primeiro campo do construtor Categoria virá nulo
+		//pois será preenchido automaticamente com o identity do Banco de Dados.
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		//Dentro do método correspondente a gravação dos objetos no banco, foi criado
+		//um arraylist para inserir os valores dos objetos instanciados.
+		repo.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 
 }
