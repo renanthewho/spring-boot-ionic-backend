0f8b394d63935ae5a9655ce5da9074ed0fd457d9
Renan Almeida <44383286+renanthewho@users.noreply.github.com>
2021-01-22 18:19:40 -0300

    Reescrevendo toString

--- src/main/java/com/renanalmeida/domain/Pedido.java
+++ src/main/java/com/renanalmeida/domain/Pedido.java
@@ -1,8 +1,11 @@
 package com.renanalmeida.domain;
 
 import java.io.Serializable;
+import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashSet;
+import java.util.Locale;
 import java.util.Set;
 
 import javax.persistence.CascadeType;
@@ -135,5 +138,29 @@ public class Pedido implements Serializable{
 			return false;
 		return true;
 	}
+
+	@Override
+	public String toString() {
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy hh:mm:ss");
+		NumberFormat nf = NumberFormat.getCurrencyInstance(new Locale("pt","BR"));
+		StringBuilder builder = new StringBuilder();
+		builder.append("Pedido número: ");
+		builder.append(getId());
+		builder.append(", Data/Hora: ");
+		builder.append(sdf.format(getDataPedido()));
+		builder.append(", Cliente: ");
+		builder.append(getCliente().getNome());
+		builder.append(", Situação do Pagamento: ");
+		builder.append(getPagamento().getEstado().getDescricao());
+		builder.append("\nDetalhes:\n");
+		for(ItemPedido itens : getItens()) {
+			builder.append(itens.toString());
+		}
+		builder.append(", Valor Total: ");
+		builder.append(nf.format(getValorTotal()));
+		return builder.toString();
+	}
+	
+	
 
 }
